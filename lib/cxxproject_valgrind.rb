8c628086af627b83e68571f710ec6b7a7b85c815
Christian Köstlin <christian.koestlin@gmail.com>
2012-06-17 18:56:45 +0200

    new plugin structure

--- lib/cxxproject_valgrind.rb
+++ lib/cxxproject_valgrind.rb
@@ -1,29 +1,27 @@
-class Valgrind
-  def self.available?
+cxx_plugin do |cxxproject2rake, building_blocks, log|
+  def available?
     return system 'which valgrind'
   end
 
-  def initialize(toRake, building_blocks, log)
-    if not Valgrind.available?
-      log.error('you should install valgrind or disable the valgrind plugin')
-      return
-    else
-      toRake.create_filter_task_with_namespace(:valgrind)
-    end
+  if not available?
+    log.error('you should install valgrind or disable the valgrind plugin')
+    return
+  else
+    cxxproject2rake.create_filter_task_with_namespace(:valgrind)
+  end
 
-    building_blocks.each_value.to_a.select do |bb|
-      bb.is_a? Cxxproject::Executable
-    end.each do |bb|
-      name = bb.name
-      executable = bb.get_task_name
-      namespace 'valgrind' do
-        desc "run executable #{executable} with valgrind"
-        res = task name => executable do |t|
-          sh "valgrind #{executable}"
-        end
-        res.type = Rake::Task::RUN
-        res
+  building_blocks.each_value.to_a.select do |bb|
+    bb.is_a? Cxxproject::Executable
+  end.each do |bb|
+    name = bb.name
+    executable = bb.get_task_name
+    namespace 'valgrind' do
+      desc "run executable #{executable} with valgrind"
+      res = task name => executable do |t|
+        sh "valgrind #{executable}"
       end
+      res.type = Rake::Task::RUN
+      res
     end
   end
 end
